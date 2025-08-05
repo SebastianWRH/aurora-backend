ca7604c7ce87ca3557d4b447b57b5d824276c69a
SebastianWRH <swenceslaorodriguezhorna@gmail.com>
2025-08-05 14:02:39 -0500

    Fix: usar process.env.PORT para Render

--- index.js
+++ index.js
@@ -263,10 +263,11 @@ app.post('/pedidos', (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
 
 
 
